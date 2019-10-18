52170d8718b74f50726cc7063df8dc40079069b8
allensam88 <54912919+allensam88@users.noreply.github.com>
2019-10-18 09:08:15 -0700

    added search feature

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -3,6 +3,7 @@ import React from 'react';
 const CharacterCard = props => {
 	return (
 		<div className="card">
+      <h2>{props.character.name}</h2>
 			<img src={props.character.image} alt="rick and morty character" />
 		</div>
 	)
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,29 +1,52 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 import CharacterCard from './CharacterCard';
 
 const CharacterList = () => {
-	const [characters, setCharacters] = useState([]);
+  const [characters, setCharacters] = useState([]);
+  const [query, setQuery] = useState('');
 
 	useEffect(() => {
-		axios.get(`https://rickandmortyapi.com/api/character/`)
+    axios
+      .get(`https://rickandmortyapi.com/api/character/`)
 			.then(response => {
-				setCharacters(response.data.results);
+        console.log(response);
+        const data = response.data.results.filter(character =>
+          character.name.toLowerCase().includes(query.toLowerCase())
+        );
+        setCharacters(data);
 			})
 			.catch(error => {
 				console.log(error);
 			});
 
-	}, []);
+	}, [query]);
+
+  const handleInputChange = event => {
+    setQuery(event.target.value);
+  };
 
 	return (
-		<div className="list">
-			{characters.map((character, index) => {
-				return <CharacterCard key={index} character={character} />
-			})}
-		</div>
+    <div>
+      <form className="search">
+        <input
+          type="text"
+          onChange={handleInputChange}
+          value={query}
+          name="name"
+          tabIndex="0"
+          className="prompt search-name"
+          placeholder="search by name"
+          autoComplete="off"
+        />
+      </form>
+      <div className="list">
+        {characters.map(character => {
+          return <CharacterCard character={character} />
+        })}
+      </div>
+    </div>
 	)
 }
 

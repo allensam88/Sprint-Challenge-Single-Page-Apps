2cbe96dc04ec87dedca9ebf211d04362ac93771a
allensam88 <54912919+allensam88@users.noreply.github.com>
2019-10-18 13:20:54 -0700

    updated SearchForm.js file

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,79 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import CharacterCard from './CharacterCard';
-import styled from 'styled-components';
-
-const StyledForm = styled.form`
-  display: flex;
-  justify-content: center;
-  margin: 25px;
-`;
-
-const StyledInput = styled.input`
-  border: 1px solid black;
-  width: 300px;
-  line-height: 30px;
-  font-size: 20px;
-  padding: 5px;
-`;
-
-const StyledCardsContainer = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-`;
+import SearchForm from './SearchForm';
 
 const CharacterList = () => {
   const [characters, setCharacters] = useState([]);
-  const [query, setQuery] = useState('');
-  const [filteredCharacter, setFilteredCharacter] = useState([]);
 
 	useEffect(() => {
     axios
       .get(`https://rickandmortyapi.com/api/character/`)
 			.then(response => {
         console.log(response);
-        const data = response.data.results
-        setCharacters(data);
-        setFilteredCharacter(data);
+        setCharacters(response.data.results);
 			})
 			.catch(error => {
 				console.log(error);
 			});
 
   }, []);
-  
-  useEffect(() => {
-    setFilteredCharacter(
-      characters.filter(character =>
-        character.name.toLowerCase().includes(query.toLowerCase())
-      )
-    );
-  }, [query, characters]);
-
-  const handleInputChange = event => {
-    setQuery(event.target.value);
-  };
 
 	return (
     <div>
-      <StyledForm className="search">
-        <StyledInput
-          type="text"
-          onChange={handleInputChange}
-          value={query}
-          name="name"
-          tabIndex="0"
-          className="prompt search-name"
-          placeholder="search by name"
-          autoComplete="off"
-        />
-      </StyledForm>
-      <StyledCardsContainer className="list">
-        {filteredCharacter.map(character => {
-          return <CharacterCard key={character.name} character={character} />
-        })}
-      </StyledCardsContainer>
+      <SearchForm characters={characters}/>
     </div>
 	)
 }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,64 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import styled from 'styled-components';
+import CharacterCard from './CharacterCard';
 
-export default function SearchForm() {
- 
+const StyledForm = styled.form`
+  display: flex;
+  justify-content: center;
+  margin: 25px;
+`;
+
+const StyledInput = styled.input`
+  border: 1px solid black;
+  width: 300px;
+  line-height: 30px;
+  font-size: 20px;
+  padding: 5px;
+`;
+
+const StyledCardsContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+const SearchForm = (props) => {
+  const [query, setQuery] = useState('');
+  const [filteredCharacter, setFilteredCharacter] = useState([props.characters]);
+
+  useEffect(() => {
+    setFilteredCharacter(
+      props.characters.filter(character =>
+        character.name.toLowerCase().includes(query.toLowerCase())
+      )
+    );
+  }, [query, props.characters]);
+
+  const handleInputChange = event => {
+    setQuery(event.target.value);
+  };
+
   return (
     <section className="search-form">
-     // Add a search form here
+      <StyledForm className="search">
+        <StyledInput
+          type="text"
+          onChange={handleInputChange}
+          value={query}
+          name="name"
+          tabIndex="0"
+          className="prompt search-name"
+          placeholder="search by name"
+          autoComplete="off"
+        />
+      </StyledForm>
+      <StyledCardsContainer className="list">
+        {filteredCharacter.map(character => {
+          return <CharacterCard key={character.name} character={character} />
+        })}
+      </StyledCardsContainer>
     </section>
   );
 }
+
+export default SearchForm;
